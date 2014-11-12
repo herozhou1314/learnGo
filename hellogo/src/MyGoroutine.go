f79146eb1b6dd11c9c24afd121c55351ce45ba40
herozhou1314 <herozhou1314@gmail.com>
2014-11-12 09:59:16 +0800

    add

--- hellogo/src/MyGoroutine.go
+++ hellogo/src/MyGoroutine.go
@@ -1,19 +1,42 @@
 package main
 
-import "fmt"
-var syn chan int = make(chan int)
-func main(){
-	fmt.Println("hello")
-	//printHello()
-	go printHello()
-	//printHello()
-//	fmt.Println("I'm listening.")
-//	time.Sleep(2 * time.Second)
-//	fmt.Println("You're boring; I'm leaving.")
-	<- syn
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan int, 50)
+	go printHello(c)
+	a := []int{7, 2, 8, -9, 4, 0}
+	//go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+
+	go func(a string, b int) {
+		time.Sleep(3 * time.Second)
+		fmt.Println("printHello", a, b)
+		c <- 5 // 将和送入 c
+	}("hello", 123)
+
+	x, ok := <-c
+	y, ok1 := <-c  // 从 c 中获取
+	y1, ok2 := <-c // 从 c 中获取
+	fmt.Println(x, y, x+y, ok, ok1, y1, ok2)
+
 }
 
-func printHello(){
+func printHello(c chan int) {
+	time.Sleep(2 * time.Second)
 	fmt.Println("printHello")
-	syn <- 1
+	c <- (-100)
+}
+func sum(a []int, c chan int) {
+	time.Sleep(1 * time.Second)
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	fmt.Println("sum")
+	c <- sum // 将和送入 c
+	//close(c)
 }
